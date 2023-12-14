77db6c86084970179a0280964b81f0a0448fe7c3
mutual99 <152463166+mutual99@users.noreply.github.com>
2023-12-14 17:55:33 +0900

    자바스크립트 object

---
+++ 3_JAVASCRIPT/object/js/실습문제1.js
@@ -0,0 +1,21 @@
+function printValue() {
+  const customer = document.getElementById("customer").value;
+  const product = document.getElementById("product").value;
+  const price = document.getElementById("price").value;
+  result1.innerHTML = `${customer}님의 구매 상품은 ${product}이고, 구매 가격은 ${price}입니다.`;
+}
+
+function stringLength() {
+  const a1 = document.getElementById("text1").value;
+  console.log(a1.length);
+  console.log(`텍스트필드에 작성한 문자의 길이는 ${a1.length}입니다.`);
+  result2.innerHTML = `보기의 문자열의 길이는 ${a1.length}입니다.`;
+}
+
+function stringSplit() {
+  const words = document.getElementById("la1");
+  console.log(words.innerHTML.split(","));
+  result3.innerHTML = `${words.innerHTML.split(",")}`;
+}
+
+function arrayTest() {}
