d785f04b55ba5a5073734a8f43b531f9de0f91be
mutual99 <152463166+mutual99@users.noreply.github.com>
2023-12-15 17:49:34 +0900

    04_이벤트 실습문제02~03 readme

---
+++ 3_JAVASCRIPT/object/js/실습문제2.js
@@ -0,0 +1,15 @@
+// const clock = document.querySelector("#clock");
+// function remaining() {
+//   const now = new Date("2024-01-01 00:00:00");
+//   const re = new Date();
+
+//   const Timeremaining = re - now;
+
+//   const hour = Math.floor((Timeremaining / (1000 * 60 * 60)) % 24);
+//   const min = Math.floor((Timeremaining / (1000 * 60)) % 60);
+//   const se = Math.floor((Timeremaining / 1000) % 60);
+
+//   console.log(Math.abs(`${hour}h ${min}m ${se}s`));
+// }
+// remaining();
+// setInterval(remaining, 1000);
